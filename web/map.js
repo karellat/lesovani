310820826bc67aa6f6c200687df3d67af1903749
Tomas Karella <tomas.karella@gmail.com>
2019-10-28 11:26:34 +0100

    Split into scripts

---
+++ web/map.js
@@ -0,0 +1,125 @@
+var z = 7.5
+var map = L.map('map', {
+    center : [49.8037633, 15.4749126],
+    zoom : z,
+    dragging : false,
+    boxZoom : false,
+    doubleClickZoom: false, 
+    zoomControl: false, 
+    minZoom: z,
+    maxZoom: z,
+});
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    id: 'mapbox.light'
+}).addTo(map);
+
+
+// control that shows state info on hover
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>ČR okresy: </h4>' +  (props ?
+        '<b>' + props.NAZ_LAU1 + '</b><br />' + props.POCET_OB_1 + ' obyvatel'
+        : 'Zvolte okres');
+};
+
+info.addTo(map);
+
+
+// get color depending on population density value
+function getColor(d) {
+    return d > 1000000 ? '#800026' :
+            d > 300000 ? '#BD0026' :
+            d > 200000  ? '#E31A1C' :
+            d > 100000  ? '#FC4E2A' :
+            d > 50000   ? '#FD8D3C' :
+            d > 20000   ? '#FEB24C' :
+            d > 10000   ? '#FED976' :
+                        '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.POCET_OB_1)
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10000, 20000, 50000, 100000, 200000, 500000, 10000000],
+        labels = [],
+        from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
